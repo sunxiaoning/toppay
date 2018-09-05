d7ff60590ebcb2e0989d0dd33fd7473b2d06f7d1
sunxiaoning <1530358579@qq.com>
2018-09-05 22:31:01 +0800

    代码优化

--- app/src/main/java/cc/williamsun/rabbitservice/toppay/task/WritePayeeMsgTask.java
+++ app/src/main/java/cc/williamsun/rabbitservice/toppay/task/WritePayeeMsgTask.java
@@ -120,12 +120,11 @@ public class WritePayeeMsgTask extends AsyncTask<Void,Void,Void>{
         bizContent.put("successTime",DateUtil.formatDate(new Date()));
         byte [] digestBizContent;
         try {
-            RSAUtils.CUSTOM_ALGORITHM = "RSA/ECB/PKCS1Padding";
+            RSAUtils.CUSTOM_ALGORITHM = "RSA/None/PKCS1Padding";
             digestBizContent = RSAUtils.encryptByPublicKey(JacksonUtil.toJson(bizContent).getBytes("UTF-8"),ENCRYPT_KEY_CONFIG.get(EncryptKeyConst.PLATFORM_PUBLIC_KEY));
         } catch (Exception e){
             throw new PlatformRuntimeException("收款通知请求参数加密出错！",e);
         }
-        Toast toast = Toast.makeText(context, Base64Utils.encode(digestBizContent), Toast.LENGTH_LONG);
         requestParam.put(GatewayPostRequestConst.BIZ_CONTENT, Base64Utils.encode(digestBizContent));
         String sign;
         try {
